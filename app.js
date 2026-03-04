15bd71970684a2b25d7c909be91f1d091229fced
Diya Sebastian <diyasebastian2003@gmail.com>
2026-03-04 22:17:27 +0530

    Implement navigation button functionality

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const navButtons = document.querySelectorAll(".nav-btn");
+const pages = document.querySelectorAll(".page");
 
+navButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        if (button.id === "darkToggle") {
+            document.body.classList.toggle("dark-mode");
+            return;
+        }
+
+        navButtons.forEach(btn => btn.classList.remove("active"));
+        pages.forEach(page => page.classList.remove("active"));
+
+        button.classList.add("active");
+
+        const pageId = button.getAttribute("data-page");
+        document.getElementById(pageId).classList.add("active");
+    });
+});
